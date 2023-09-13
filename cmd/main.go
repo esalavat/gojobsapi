e3a406d53fef4ba50cffb22761f4e904684dad07
Eric Salavatcioglu <eric.salavat@gmail.com>
2023-09-13 12:25:50 -0700

    Changed docker config to connect to db and debug table query in go.

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,48 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"fmt"
+	"os"
+	"time"
 
-func main() {
-    app := fiber.New()
+	"github.com/gofiber/fiber/v2"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
 
-    app.Get("/", func(c *fiber.Ctx) error {
-        return c.SendString("Hello, world!")
-    })
-
-    app.Listen(":3000")
+type Job struct {
+	Id          int       `gorm:"id"`
+	Company     string    `gorm:"company"`
+	JobTitle    string    `gorm:"job_title"`
+	DateApplied time.Time `gorm:"date_applied"`
+	JobUrl      string    `gorm:"job_url"`
 }
 
+func main() {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		dsn := fmt.Sprintf(
+			"host=jobs_db user=%s password=%s dbname=%s port=5432 sslmode=disable TimeZone=Asia/Shanghai",
+			os.Getenv("DB_USER"),
+			os.Getenv("DB_PASSWORD"),
+			os.Getenv("DB_NAME"),
+		)
+
+		db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+		if err != nil {
+			panic("Failed to connect database.")
+		}
+
+		var job Job
+
+		db.First(&job, 1)
+
+		str := job.JobTitle
+
+		return c.SendString(str)
+	})
+
+	app.Listen(":3000")
+
+}
